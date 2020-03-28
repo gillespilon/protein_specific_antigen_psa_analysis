9060e020e13bad27e9cb00165f5f1468d12d55c2
Gilles Pilon <gillespilon13@gmail.com>
2020-03-28 15:59:29 -0500

    Delete register_matplotlib_converters, dos not seem to be required

--- psa_analysis.py
+++ psa_analysis.py
@@ -25,7 +25,6 @@ from matplotlib.ticker import NullFormatter, NullLocator
 
 matplotlib.use('Cairo')
 c = cm.Paired.colors
-pd.plotting.register_matplotlib_converters(explicit=True)
 
 
 def main():
