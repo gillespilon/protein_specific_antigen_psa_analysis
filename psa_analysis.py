ebadc952575198e8c8c8bbffe7eb1c09f3c8e553
Gilles Pilon <gilles.pilon@gillespilon.ca>
2021-08-26 15:43:23 -0500

    Add references

--- psa_analysis.py
+++ psa_analysis.py
@@ -8,6 +8,23 @@ the PSA will reach the warning limits.
 
 time -f '%e' ./psa_analysis.py
 ./psa_analysis.py
+
+Balk, S.P. Y.J. Ko, and G.J. Bubley.
+"Biology of prosate-specific antigen."
+*Journal of Clinical Oncology* 21 (January 2003), no. 2: 383-91.
+[PMID 12525533 (https://www.ncbi.nlm.nih.gov/pubmed/12525533)]
+(https://www.ncbi.nlm.nih.gov/pubmed/12525533).
+[doi:10.1200/JCO.2003.02.083 (https://doi.org/10.1200 2FJCO.2003.02.083)]
+(https://doi.org/10.1200/JCO.2003.02.083).
+
+Catalona, W.J., J.P. Richie, F.R. Ahmann, M.A. Hudson, P.T. Scardino,
+R.C. Flanigan, J.B. deKernion, T.L. Ratliff, L.R. Kavoussi, and B.L. Dalkin.
+"Comparison of digital rectal examination and serum prostate specific antigen
+in the early detection of prostate cancer: results of a multicenter clinical
+trial of 6,630 mean."
+*The Journal of Urology* 151 (May 1994), no. 5: 1283-90.
+[PMID 7512659 (https://www.ncbi.nlm.nih.gov/pubmed/7512659)]
+(https://www.ncbi.nlm.nih.gov/pubmed/7512659).
 '''
 
 from typing import Tuple
